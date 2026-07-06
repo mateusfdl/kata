af8797aa7e751f35bc094b762a0bbbe35f3a8fdb
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:18:03 -0300

    refactor(rules): port go/max-function-length to kata

--- rules/go/max-function-length.scm
+++
@@ -1,5 +0,0 @@
-([(function_declaration)
-  (method_declaration)
-  (func_literal)] @match
- (#where? "(> (length @match) 50)")
- (#set! message "function spans {length @match} lines (max 50) - split it into focused units"))
