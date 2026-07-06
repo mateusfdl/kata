40ee7df267823872063d63b05ff982beb2aa726e
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:11:50 -0300

    refactor(rules): port tsx/no-console to kata

--- rules/tsx/no-console.scm
+++
@@ -1,5 +0,0 @@
-((call_expression
-  function: (member_expression
-    object: (identifier) @obj)) @match
- (#eq? @obj "console")
- (#set! message "console is not allowed - use proper instrumentation"))
