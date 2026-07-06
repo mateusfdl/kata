38842a0e4b855e66d9cea90cdebe58440ceac562
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:17:48 -0300

    refactor(rules): port go/max-complexity to kata

--- rules/go/max-complexity.scm
+++
@@ -1,5 +0,0 @@
-([(function_declaration)
-  (method_declaration)
-  (func_literal)] @match
- (#where? "(> (complexity @match) 10)")
- (#set! message "cyclomatic complexity {complexity @match} exceeds 10 - extract smaller functions"))
