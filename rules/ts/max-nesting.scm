e530da489677e3c6f3d5d747987e163a002138d5
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 14:01:42 -0300

    refactor(rules): port ts/max-nesting to kata

--- rules/ts/max-nesting.scm
+++
@@ -1,8 +0,0 @@
-([(function_declaration)
-  (function_expression)
-  (generator_function_declaration)
-  (generator_function)
-  (arrow_function)
-  (method_definition)] @match
- (#where? "(> (nesting @match) 3)")
- (#set! message "nesting depth {nesting @match} exceeds 3 - flatten with guard clauses"))
