c301a09f9d69b140943a3ea4dcfb0028b526de08
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:12:36 -0300

    refactor(rules): port tsx/no-as-any to kata

--- rules/tsx/no-as-any.scm
+++
@@ -1,7 +0,0 @@
-((as_expression (predefined_type) @t) @match
- (#eq? @t "any")
- (#set! message "as any is not allowed - use a proper type to represent it"))
-
-((as_expression (array_type (predefined_type) @t)) @match
- (#eq? @t "any")
- (#set! message "as any[] is not allowed - use a proper type to represent it"))
