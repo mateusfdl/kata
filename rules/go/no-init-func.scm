4b6aed53b1ed21b457a6bdf050dbf9d02551c57b
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:19:19 -0300

    refactor(rules): port go/no-init-func to kata

--- rules/go/no-init-func.scm
+++
@@ -1,4 +0,0 @@
-((function_declaration
-  name: (identifier) @name) @match
- (#eq? @name "init")
- (#set! message "init functions hide setup and resist testing - use an explicit constructor and dependency injection"))
