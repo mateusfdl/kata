912f8a9450f2b8ef01169f3f010899c22457c96c
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:17:18 -0300

    refactor(rules): port go/context-first-param to kata

--- rules/go/context-first-param.scm
+++
@@ -1,9 +0,0 @@
-((parameter_list
-  (parameter_declaration)
-  (parameter_declaration
-    type: (qualified_type
-      package: (package_identifier) @pkg
-      name: (type_identifier) @typ)) @match)
- (#eq? @pkg "context")
- (#eq? @typ "Context")
- (#set! message "context.Context must be the first parameter of a function"))
