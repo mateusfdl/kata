7af0d8af2554f79e0285ca399b29e9e974c57c34
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:13:53 -0300

    refactor(rules): port tsx/simple-repositories to kata

--- rules/tsx/simple-repositories.scm
+++
@@ -1,6 +0,0 @@
-((class_declaration
-   name: (type_identifier) @name
-   body: (class_body (method_definition) @match))
- (#match? @name "Repository$")
- (#where? "(> (complexity @match) 5)")
- (#set! message "repository method complexity {complexity @match} exceeds 5 - repositories must stay simple"))
