c794d24cdb8c8fc74b39df0c82682c765091553a
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:21:44 -0300

    refactor(rules): port go/simple-repositories to kata

--- rules/go/simple-repositories.scm
+++
@@ -1,6 +0,0 @@
-((method_declaration
-   receiver: (parameter_list (parameter_declaration
-     type: [(type_identifier) @recv (pointer_type (type_identifier) @recv)]))) @match
- (#match? @recv "Repository$")
- (#where? "(> (complexity @match) 5)")
- (#set! message "repository method complexity {complexity @match} exceeds 5 - repositories must stay simple"))
