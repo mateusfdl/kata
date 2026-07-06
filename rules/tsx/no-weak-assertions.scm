bc4564d642f1a63bb76455207df8829073ba1223
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:12:51 -0300

    refactor(rules): port tsx/no-weak-assertions to kata

--- rules/tsx/no-weak-assertions.scm
+++
@@ -1,8 +0,0 @@
-((call_expression
-  function: (member_expression
-    object: (call_expression
-      function: (identifier) @expect)
-    property: (property_identifier) @name)) @match
- (#eq? @expect "expect")
- (#any-of? @name "toBeDefined" "toBeUndefined" "toBeNull" "toBeTruthy" "toBeFalsy" "toHaveBeenCalled" "toContain")
- (#set! message "weak assertion - use .toEqual() with explicit values"))
