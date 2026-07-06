c8165435b124bf55650ed8fab794bbaa2357bf91
Matheus Lima <matheus.limastack@gmail.com>
2026-07-06 15:18:49 -0300

    refactor(rules): port go/no-builtin-print to kata

--- rules/go/no-builtin-print.scm
+++
@@ -1,4 +0,0 @@
-((call_expression
-  function: (identifier) @fn) @match
- (#any-of? @fn "print" "println")
- (#set! message "builtin print/println is debug-only and not guaranteed by the spec - use proper instrumentation"))
