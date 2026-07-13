51c7dddf9dbdddc41a92ca429d2ff15c8408f246
Matheus Lima <matheus.limastack@gmail.com>
2026-07-13 15:34:41 -0300

    test(rules): exercise value receivers in go simple-repositories

--- rules/go/tests/simple-repositories.go
+++ rules/go/tests/simple-repositories.go
@@ -19,6 +19,23 @@ func (r *UserRepository) FindActive(a, b, c bool) int {
 	return 5
 }
 
+// kata-expect: simple-repositories
+func (r UserRepository) CountActive(a, b, c bool) int {
+	if a {
+		return 1
+	}
+	if b {
+		return 2
+	}
+	if c {
+		return 3
+	}
+	if a && b {
+		return 4
+	}
+	return 5
+}
+
 type OrderService struct{}
 
 func (s *OrderService) Create(a bool) int {
