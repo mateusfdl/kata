56b6bee8b73ff3ea77a6245f47f5d505a331d504
Matheus Lima <matheus.limastack@gmail.com>
2026-07-24 04:21:49 -0300

    chore(nix): bump flake to 1.10.0

--- flake.nix
+++ flake.nix
@@ -203,7 +203,7 @@
         {
           default = pkgs.stdenv.mkDerivation (finalAttrs: {
             pname = "kata";
-            version = "1.9.0";
+            version = "1.10.0";
             src = self;
 
             zigDeps = zig.fetchDeps {
